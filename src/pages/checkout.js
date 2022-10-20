89ef31d2e0f727a0905f66ff72b547aafef44f25
sagun.panthi7@gmail.com <sagunpanthi>
2022-10-21 00:08:00 +1100

    checkout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Header from '../components/Header'
 import Image from "next/image"
 import { useSelector } from 'react-redux'
-import { selectItems } from '../slices/basketSlice'
+import { selectItems, selectTotal } from '../slices/basketSlice'
 import CheckoutProduct from '../components/CheckoutProduct'
+import Currency from 'react-currency-formatter'
+import { useSession } from 'next-auth/react'
 
 const Checkout = () => {
-  const items = useSelector(selectItems)
+  const items = useSelector(selectItems);
+  const total = useSelector(selectTotal);
+  const { data: session } = useSession();
 
   return (
     <div className='bg-gray-100'>
@@ -24,7 +28,7 @@ const Checkout = () => {
          
           <div className='flex flex-col p-5 space-y-10 bg-white'>
             <h1 className='text-3xl border-b pb-4'>{items.length === 0? 'Your basket is empty': "Shopping basket"}</h1>
-            <h1>
+           
               {items.map((item,i)=>(
                     <CheckoutProduct 
                       key={i}
@@ -39,10 +43,29 @@ const Checkout = () => {
 
                     />
               ))}
-            </h1>
+         
           </div>
 
-        
+        <div className='flex flex-col bg-white p-10 shadow-md'>
+                {items.length > 0 && (
+                  <div>
+                    <h2 className='whitespace-nowrap'>Subtotal ({items.length} items)
+                      <span className='font-bold'> 
+                        <Currency quantity={total} currency="GBP" />
+                      </span>
+
+                    </h2>
+                    <button
+                      disabled={!session}
+                    className= {`button mt-2 ${!session && `from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed` }`}>
+                      {!session ? 'Sign in to checkout':'Proceed to checkout'}
+                    </button>
+                  </div>
+                 
+                  
+                 
+                ) }
+        </div>
         
         
         </main>
